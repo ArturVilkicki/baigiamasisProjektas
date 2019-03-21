28f27a1db902147118f35146fcb29591be70d8b3
ArturVilkicki <30698622+ArturVilkicki@users.noreply.github.com>
2019-03-21 19:04:31 +0200

    i navbar ideti products,categories prisiloginus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,38 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
+
                         @else
+                             <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true" href="{{url('admin/products')}}">Products
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    
+                                    <a href="{{url('admin/products/create')}}">Create</a>
+                                    <br>
+                                    <a href="{{url('admin/products')}}">List</a>
+                                    <br>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="true" href="{{url('admin/categories')}}">Categories
+                                    <span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        
+                                        <a href="{{url('admin/categories/create')}}">Create</a>
+                                        <br>
+                                        <a href="{{url('admin/categories')}}">List</a>
+                                        <br>
+                                    </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
+                                    
+                                </div>
+                            </li> 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
